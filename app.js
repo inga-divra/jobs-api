f495d2eea1d472e9287e709770ba8cf191927b06
inga-divra <ingady@gmail.com>
2025-04-05 11:03:37 +0300

    Add root route to check API status

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('Jobs API is running...');
+});
+
 //routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
